e474967af3449e4b2b3f0614c2761fe86ea62053
Krishna <143727212+devansh-pan@users.noreply.github.com>
2024-12-26 08:05:26 +0000

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<h1>Welcome to SvelteKit</h1>
+<a href="/auth" >Login</a>
+<a href="/dashboard">Dashboard</a>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
