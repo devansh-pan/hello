4a506aa90dec4119ce38d5293f257a2f47f5d195
Krishna <143727212+devansh-pan@users.noreply.github.com>
2024-12-29 12:48:15 +0000

    success

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -3,7 +3,7 @@
   import type { EventHandler } from 'svelte/elements'
 
   import type { PageData } from './$types'
-
+ let message = $state("");
   let { data } = $props()
   let { profile, supabase, user } = $derived(data)
 console.log(data)
@@ -13,29 +13,50 @@ console.log(data)
 
     const form = evt.target as HTMLFormElement
 
-    const note = (new FormData(form).get('note') ?? '') as string
-    if (!note) return
-
-    const { error } = await supabase.from('notes').insert({ note })
+    const file = (new FormData(form).get('avatar') ?? '') as string
+    const fullname = (new FormData(form).get('fullname') ?? profile.full_name) as string
+    if (fullname && file) {
+      const {data:path ,error:fileError} = await supabase.storage.from('avatars').upload(profile.username,file,{upsert: true})
+      if (fileError) {
+        return message = fileError.message
+      }
+ const { data , error} = await supabase.from('profiles').upsert({id:profile.id,full_name: fullname,avatar_url:path.fullPath}).eq('id', profile.id);
+      if (error) {
+        console.log(error)
+      }
+      return message = "Profile updated"
+      form.reset()
+    } 
+    if (note) {
+const { error } = await supabase.from('notes').insert({ note })
     if (error) console.error(error)
 
-    invalidate('supabase:db:notes')
-    form.reset()
+    invalidate('supabase:db:profiles')
+    form.reset() }
   }
+  const {data:url} = supabase.storage.from('avatars').getPublicUrl(profile.username)
+const avatar = url.publicUrl
+console.log(url.publicUrl)
+  
 </script>
 {#if user}
-<h1>Welcome {user?.username}</h1>
+<h1>Welcome {user?.id}</h1>
 <ul class="list text-blue-500 text-md">
   {#if profile}
     <li>User name: {profile.username}</li>
+    <li>Full name: {profile.full_name}</li>
     <li>Email: {user.email}</li>
   {/if}
 </ul>
+  <img src={avatar} alt="avatar" class="aspect-square w-[60px] rounded circle" />
   <a href="/user/{profile.username}">View public profile</a>
 <form onsubmit={handleSubmit}>
-  <label>
-    Add a note
-    <input name="note" type="text" />
+  <label for="fullname">
+    Update name
+    <input id="fullname" name="fullname" type="text" value={profile.full_name} />
   </label>
+  <label for="avatar" >Avatar</label>
+  <input id="avatar" name="avatar" type="file" accept="image/*" >
 </form>
+  <p>{message}</p>
 {/if}
--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: [vitePreprocess({ script: true }), mdsvex({extensions:'mdx','svx','md'})],
+	preprocess: [vitePreprocess({ script: true }), mdsvex()],
 
 	kit: {
 		csrf: {
