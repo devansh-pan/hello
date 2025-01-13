9b98bc5b28cfd65ef11b4771ebc53da49608447c
Krishna <143727212+devansh-pan@users.noreply.github.com>
2025-01-13 08:53:00 +0000

    Ghiii

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -9,9 +9,9 @@
 <header class="flex bg-green-700 text-white p-4 w-full border-b dark:bg-green-950/70 dark:border-gray-600 flex-row ">
   <a href="/">
   {#if page.url.pathname === '/'}
-  <h1 class="text-3xl mt-[10px] font-bolder text-pink-100/80 dark:text-pink-100">Telugu18</h1>
+  <h1 class="text-3xl mt-[10px] font-bolder text-pink-100/80 dark:text-pink-100">Prabha</h1>
   {:else}
-    <p class="text-3xl mt-[10px] font-bold text-pink-100/80 dark:text-pink-100">Telugu18</p>
+    <p class="text-3xl mt-[10px] font-bold text-pink-100/80 dark:text-pink-100">Prabha</p>
   {/if}
   </a>
   <Darkmode />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,10 +4,9 @@ const {data: meta} = $derived(data)
   const {user} = $derived(meta)
 </script>
 <svelte:head>
-  <title>Telugu1</title>
-  <meta name="description" content="Welcome to Telugu1 " >
+  <title>Prabha</title>
+  <meta name="description" content="Welcome to prabha " >
   </svelte:head>
-<h2 class="h1 text-xl m-1 mt-3">Welcome {user?.user_metadata?.full_name ?? "Guest"}</h2>
 <section class="flex dark:bg-slate-900 dark:text-slate-200 flex-row border-b shadow p-1 m-1 rounded">
 </section>
 
