fd7c4f7db78795f2e2ec8c1a4a874d24f7f83aaf
Krishna <143727212+devansh-pan@users.noreply.github.com>
2025-02-14 09:26:58 +0000

    Hdggh

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,60 +1,62 @@
 <script lang="ts">
 	import Quiz from '$lib/Quiz.svelte';
-	import {invalidate} from "$app/navigation"
-	import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from "$env/static/public"
+	import { invalidate } from '$app/navigation';
+	import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 	import { supabase } from '$lib/supabase';
 	import { marked } from 'marked';
 	let md = $state('');
 	let title = $state('');
-	 let content = $derived(marked.parse(md));
+	let content = $derived(marked.parse(md));
 	let message = $state();
-	let id = $state(crypto.randomUUID())
-	let posts : any[] = $state([]);
+	let id = $state(crypto.randomUUID());
+	let posts: any[] = $state([]);
 	const postsFetch = async () => {
 		let { data, error } = await supabase.from('posts').select('*');
 		if (!error) {
-			posts = data ?? [{}]
+			posts = data ?? [{}];
 			console.log(posts);
 		}
 	};
 	postsFetch();
-	async function post(event: SubmitEvent & { currentTarget: EventTarget & HTMLFormElement } ) {
+	async function post(event: SubmitEvent & { currentTarget: EventTarget & HTMLFormElement }) {
 		event.preventDefault();
 		if (title === '' || md === '') {
 			return (message = 'Cannot be empty ');
 		}
 		const { data, error } = await supabase
 			.from('posts')
-			.upsert([{ id:id,title: title, content: md }]);
+			.upsert([{ id: id, title: title, content: md }]);
 		if (error) {
 			message = error.message;
 			console.log(error.message);
 		} else {
 			message = 'Posted successfully';
-			const existingPostIndex = posts.findIndex(post => post.id === id);
+			const existingPostIndex = posts.findIndex((post) => post.id === id);
 			if (existingPostIndex !== -1) {
-				posts[existingPostIndex] = {id, title, content: md};
+				posts[existingPostIndex] = { id, title, content: md };
 			} else {
-				posts.push({id, title, content: md});
+				posts.push({ id, title, content: md });
 			}
-			title = ''; md = ''; id = crypto.randomUUID();
+			title = '';
+			md = '';
+			id = crypto.randomUUID();
 			setTimeout(() => {
 				message = '';
-			}, 9000);
-			
+			}, 3000);
 		}
-		console.log({ data});
+		console.log({ data });
 	}
 
-	async function deletePost(id:string) : Promise<null>{
-		 // event.preventDefault()
-	let {data,error} = await supabase.from('posts').delete().eq('id', id).select('*');
-		if (!error){
-			message = "Deleted successfully"
+	async function deletePost(id: string): Promise<null> {
+		// event.preventDefault()
+		let { data, error } = await supabase.from('posts').delete().eq('id', id).select('*');
+		if (!error) {
+			message = 'Deleted successfully';
 		}
-		console.log({error,data});
-		invalidate(PUBLIC_SUPABASE_URL+"/rest/v1/posts");posts = posts.filter(post => post.id !== id);
-		 return null;
+		console.log({ error, data });
+		invalidate(PUBLIC_SUPABASE_URL + '/rest/v1/posts');
+		posts = posts.filter((post) => post.id !== id);
+		return null;
 	}
 </script>
 
@@ -64,22 +66,32 @@
 </svelte:head>
 <h1 class="text-3xl font-bold">Welcome to Sveltz</h1>
 <br />
-{#if posts !== null}
-	{#each posts as post, i}
-		<div class="flex flex-col">
-			<h2 class="text-2xl m-2">{post.title}</h2>
-			{post.id}
-			<p>{post.content}</p>
-		</div>
-		<button class="border p-2 m-2 " onclick={() => deletePost(post.id)}>Delete post</button>
-		<button onclick={() => {title=post.title,md = post.content ,id = post.id}} class="p-2 m-2 border">Update</button>
-	{/each}
+{#if posts !== null}<div class="flex flex-col">
+		{#each posts as post, i}
+			<div class="flex flex-col">
+				<div class="border p-2">
+					<h2 class="m-2 text-2xl">{post.title}</h2>
+					
+					<p class="**:appearence-none">{@html marked.parse(post.content)}</p>
+					<button class="m-2 w-25 border p-2" onclick={() => deletePost(post.id)}
+						>Delete post</button
+					>
+					<button
+						onclick={() => {
+							(title = post.title), (md = post.content), (id = post.id);
+						}}
+						class="m-2 w-25 border p-2">Update</button
+					>
+				</div>
+			</div>
+		{/each}
+	</div>
 {/if}
-<br>
+<br />
 {message ?? ''}
 <br />
-<div class="prose p-2 m-2 min-h-25 prose-h1:text-3xl border">
-{@html md}	
+<div class="prose prose-h1:text-3xl m-2 min-h-25 border p-2">
+	{@html marked.parse(md)}
 </div>
 <form method="POST" onsubmit={post} action="/?post">
 	<label for="title">
@@ -97,17 +109,28 @@
 	<br />
 	<br />
 	<label for="content">
-		Post content :
+		Post content : MD supported
 		<textarea
 			rows="2"
-			class="field-content [field-sizing:content] h-25 w-full border p-2"
-			placeholder="Write something md supported"
+			class="field-content [field-sizing:content] min-h-25 w-full border p-2"
+			placeholder="Write something"
 			id="content"
 			name="content"
 			bind:value={md}
 		>
 		</textarea>
 	</label>
-	<button disabled={md === '' || title === ''} formaction="/" class="m-4 rounded-lg border p-2 text-2xl disabled:text-gray-500" type="submit">Post</button>
-	<button onclick={() => {id = crypto.randomUUID(),md = '', title = ''}} class="border m-2 p-2 ">Discard</button>
+
+	<button
+		disabled={md === '' || title === ''}
+		formaction="/"
+		class="m-4 rounded-lg border p-2 text-2xl disabled:text-gray-500"
+		type="submit">Post</button
+	>
+	<button
+		onclick={() => {
+			(id = crypto.randomUUID()), (md = ''), (title = '');
+		}}
+		class="m-2 border p-2">Discard</button
+	>
 </form>
