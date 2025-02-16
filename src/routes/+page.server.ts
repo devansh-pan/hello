04aa0e4be1be1583cebb8c93110d17918f7f5413
Krishna <143727212+devansh-pan@users.noreply.github.com>
2025-02-16 13:42:59 +0000

    he

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,7 @@
 import type {PageServerLoad} from './$types'
 import {supabase} from '$lib/supabase'
-export const load:PageServerLoad = async () => {
-  let posts = await supabase.from('posts').select('*') ?? []
+export const load: PageServerLoad = async () => {
+  const posts = await supabase.from('posts').select('*').neq("deleted " , true).order("posted_at") ?? []
   return {
     posts
   }
